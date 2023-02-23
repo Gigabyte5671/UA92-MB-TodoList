b256f7392a8d2fcda3bc356d92df33ded179fab6
Giga <gigabyte5671@gmail.com>
2023-02-24 00:21:04 +1300

    Create first draft of the Todo List app

---
+++ src/App.vue
@@ -0,0 +1,136 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ListItem from './components/ListItem.vue';
+
+interface Item {
+	title: string,
+	date: string,
+	description: string,
+	colour: string,
+	selected: boolean
+}
+
+export default defineComponent({
+	name: 'App',
+	components: {
+		ListItem
+	},
+	data() {
+		return {
+			todoList: [] as Item[],
+			isDragging: false,
+			draggedItem: undefined as number | undefined,
+			dragPosition: {
+				x: 0,
+				y: 0,
+				offsetX: 0,
+				offsetY: 0
+			}
+		};
+	},
+	methods: {
+		addItem() {
+			this.todoList.push({
+				title: 'Default',
+				date: '2023-02-18',
+				description: 'Hello World!',
+				colour: '',
+				selected: false
+			});
+		},
+		removeItem() {
+			this.todoList = this.todoList.filter(item => !item.selected);
+		},
+		dragStart(index: number, event: MouseEvent) {
+			console.log(event.target, event.offsetX, event.offsetY);
+			this.isDragging = true;
+			this.draggedItem = index;
+			this.dragPosition.offsetX = event.offsetX;
+			this.dragPosition.offsetY = event.offsetY;
+		},
+		dragEnd() {
+			this.isDragging = false;
+			this.draggedItem = undefined;
+		},
+		dragMove(event: MouseEvent) {
+			this.dragPosition.x = event.x - this.dragPosition.offsetX;
+			this.dragPosition.y = event.y - this.dragPosition.offsetY;
+		}
+	},
+	watch: {
+		todoList: {
+			handler(newValue) {
+				window.localStorage.setItem(
+					'todoList',
+					JSON.stringify(newValue)
+				);
+			},
+			deep: true
+		}
+	},
+	mounted() {
+		this.todoList = JSON.parse(window.localStorage.getItem('todoList') ?? '[]');
+	}
+});
+</script>
+
+<template>
+	<h1>Todo</h1>
+
+	<ul class="controls">
+		<li>
+			<button @click="addItem()">Add</button>
+		</li>
+		<li>
+			<button @click="removeItem()">Remove</button>
+		</li>
+	</ul>
+
+	<ol
+		id="list"
+		@mousemove="dragMove($event)"
+		@mouseup="dragEnd()"
+	>
+		<ListItem
+			v-for="(item, index) of todoList"
+			v-model:title="item.title"
+			:date="item.date"
+			v-model:description="item.description"
+			v-model:colour="item.colour"
+			v-model:selected="item.selected"
+
+			@dragHandleDown="dragStart(index, $event)"
+			@dragHandleUp="dragEnd()"
+			@dragHandleMove=""
+
+			:style="{
+				pointerEvents: isDragging ? 'none' : 'all',
+				userSelect: isDragging ? 'none' : 'revert',
+				opacity: draggedItem === index ? '0' : '1'
+			}"
+		></ListItem>
+	</ol>
+
+	
+	<ListItem
+		v-if="draggedItem !== undefined"
+
+		v-model:title="todoList[draggedItem ?? -1].title"
+		:date="todoList[draggedItem ?? -1].date"
+		v-model:description="todoList[draggedItem ?? -1].description"
+		v-model:colour="todoList[draggedItem ?? -1].colour"
+		v-model:selected="todoList[draggedItem ?? -1].selected"
+
+		:style="{
+			pointerEvents: 'none',
+			boxShadow: '0px 3px 25px -7px #0017',
+			position: 'absolute',
+			top: `${dragPosition.y}px`,
+			left: `${dragPosition.x}px`,
+			zIndex: '50'
+		}"
+	></ListItem>
+</template>
+
+<style>
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,110 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'ListItem',
+	props: {
+		title: { type: String, required: true },
+		date: { type: String, required: true },
+		description: { type: String, required: true },
+		colour: { type: String, required: true },
+		selected: { type: Boolean, required: true }
+	},
+	emits: [
+		'update:title',
+		'update:description',
+		'update:colour',
+		'update:selected',
+		'dragHandleDown',
+		'dragHandleUp',
+		'dragHandleMove'
+	]
+});
+</script>
+
+<template>
+	<li
+		@mousedown="$emit('dragHandleDown', $event)"
+		@mouseup="$emit('dragHandleUp')"
+		@mousemove="$emit('dragHandleMove', $event)"
+	>
+		<input
+			type="checkbox"
+			name="selectItem"
+			:value="selected"
+			@input="$emit('update:selected', ($event.target as HTMLInputElement).value)"
+			@mousedown.stop=""
+		>
+		<h2>
+			<input
+				type="text"
+				:value="title"
+				@input="$emit('update:title', ($event.target as HTMLInputElement).value)"
+				@mousedown.stop=""
+			>
+		</h2>
+		<time>{{ date }}</time>
+		<div class="handle"></div>
+		<p>
+			<input
+				type="text"
+				:value="description"
+				@input="$emit('update:description', ($event.target as HTMLInputElement).value)"
+				@mousedown.stop=""
+			>
+		</p>
+	</li>
+</template>
+
+<style scoped>
+li {
+	display: grid;
+	grid-template-columns: 2rem 1fr 1fr;
+	grid-template-rows: 2rem auto;
+	place-items: center;
+	gap: 1ch;
+	margin-bottom: 1rem;
+	padding: 0.7ch 0.7ch 2ch;
+	background-color: white;
+	border-radius: 0.5rem;
+	box-shadow: 0px 3px 20px -7px #000;
+	cursor: grab;
+}
+
+li input[type="text"] {
+	font-family: inherit;
+	font-size: inherit;
+	font-weight: inherit;
+}
+
+li input[type="checkbox"] {
+	width: 1.2rem;
+	height: 1.2rem;
+	margin: unset;
+}
+
+li h2 {
+	width: 100%;
+	margin: unset;
+	font-size: larger;
+}
+
+li time {
+	justify-self: flex-end;
+}
+
+li p {
+	grid-area: 2 / 2 / 3 / 4;
+	width: 100%;
+	margin: unset;
+}
+
+li .handle {
+	width: 1.5rem;
+	height: 2rem;
+	background: radial-gradient(circle at center, black 1px, transparent 2px);
+	background-size: 50% 33%;
+	cursor: grab;
+	user-select: none;
+}
+</style>
